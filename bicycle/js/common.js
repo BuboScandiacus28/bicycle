295c0ed5333143541dbc5597c060c0b3318ec948
Alex Korolenko <28filosof28@gmail.com>
2019-03-25 02:50:03 +0300

    roll back

--- bicycle/js/common.js
+++ bicycle/js/common.js
@@ -85,27 +85,6 @@ $(document).ready(function() {
 			$(".cancel").css('display', 'none');
 		}
 	});
-
-	//форма отправки
-	/*$(".ajax-contact-form").submit(function() {
-		var str = $(this).serialize();
-		 
-		$.ajax({
-			type: "POST",
-			url: "https://buboscandiacus28.github.io/bicycle/mail.php",
-			data: str,
-			success: function(msg) {
-				if(msg == 'OK') {
-					result = '<p>Ваш заказ принят</p>';
-					$(".fields").hide();
-				} else {
-					result = msg;
-				}
-				$('.note').html(result);
-			}
-		});
-		return false;
-	});*/
 });
 
 //прелоадер
